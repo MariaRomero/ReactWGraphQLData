24c380af5ca429dbcf31f1b58e8143ca89795ba5
mromero <maria.romero@news.co.uk>
2019-11-08 23:25:38 +0000

    displaying list of pokemons

--- react-data-app/src/App.js
+++ react-data-app/src/App.js
@@ -1,25 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+
+const GET_POKEMON_INFO = gql`
+  {
+    pokemons(first: 151) {
+      id
+      image
+      number
+      name
+    }
+  }
+`;
 
 function App() {
+  const { data, loading, error } = useQuery(GET_POKEMON_INFO);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error, {error}</p>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <h1>Pokemon List</h1>
+      <div className="container">
+        {data &&
+          data.pokemons &&
+          data.pokemons.map((pokemon, index) => (
+            <div key={index} className="card">
+              <img src={pokemon.image} />
+              <div class="card-body">
+                <h3>{pokemon.name}</h3>
+              </div>
+            </div>
+          ))}
+      </div>
+    </React.Fragment>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
