5911ea11c1c298e31cd4f175ab7f8505f075f902
mromero <maria.romero@news.co.uk>
2019-11-08 22:36:36 +0000

    adding apollo server

---
+++ react-data-app/src/server/index.js
@@ -0,0 +1,13 @@
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: "https://graphql-pokemon.now.sh/"
+});
+
+const client = new ApolloClient({
+  cache,
+  link
+});
